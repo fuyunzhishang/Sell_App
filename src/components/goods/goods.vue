a168381f6b2e87a555595ff425941c84a45b8e2b
fuyun <fuyunzhishang@163.com>
2017-03-24 23:04:57 +0800

    ddd

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods">
+    I am goods
+  </div>
+</template>
+
+<script type="text/ecmasript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>I am ratings</div>
+</template>
+
+<script type="text/ecmasript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
